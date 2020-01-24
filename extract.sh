b22a8156a4837cf6a799c849c107550e42c957bb
Andrew Marcuse <fileformat@gmail.com>
2020-01-24 11:29:45 -0500

    extract script

---
+++ extract.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# download the latest unicode data files
+#
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo "INFO: extract starting at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
+
+TMP_DIR=./tmp
+if [ ! -d "${TMP_DIR}" ]; then
+    echo "ERROR: temporary directory ${TMP_DIR} does not exist!"
+    exit 1
+fi
+
+OUTPUT_DIR=./output
+if [ -d "${OUTPUT_DIR}" ]; then
+    echo "INFO: using output directory ${OUTPUT_DIR}"
+else
+    echo "INFO: creating output directory ${OUTPUT_DIR}"
+    mkdir -p ${OUTPUT_DIR}
+fi
+
+FILES=( Blocks.txt DerivedAge.txt EmojiSources.txt Index.txt NamesList.txt NormalizationTest.txt PropList.txt UnicodeData.txt )
+for FILE in "${FILES[@]}"
+do 
+    if [ -f "${OUTPUT_DIR}/${FILE}" ]; then
+        echo "WARNING: ${FILE} has already been extracted"
+    else
+        echo "INFO: extracting ${FILE}"
+        unzip \
+            -b \
+            -q \
+            ${TMP_DIR}/UCD.zip \
+            ${FILE} \
+            -d ${OUTPUT_DIR}
+    fi
+done
+
+echo "INFO: extract complete at $(date -u +%Y-%m-%dT%H:%M:%SZ)"
